da53ac5cee1814f333f28675e0426cad41658b8e
missdobis <missdobis@gmail.com>
2023-09-03 02:47:12 +0100

    Google-SignIn, Database-User-SetUp, Create-Prompt-Page, Create-Prompt, New-Prompt, Prompt-Form, Prompt-Routing, Prompt-APIs, Prompt-Models, Feed, Prompt-display, SearchInput, SearchText

--- utils/database.js
+++ utils/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 let isConnected = false; // track the connection
 
-export const connectDB = async () => {
+export const connectToDB = async () => {
   mongoose.set('strictQuery', true);
 
   if (isConnected) {
